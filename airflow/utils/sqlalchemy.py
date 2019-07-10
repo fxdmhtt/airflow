cf88f7bc7bbd3e9bf110e98f025759a96c130235
root <root@Dev33.localdomain>
2019-07-10 18:36:24 +0800

    change utc to local timezone

--- airflow/utils/sqlalchemy.py
+++ airflow/utils/sqlalchemy.py
@@ -32,6 +32,14 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 
 log = LoggingMixin().log
 utc = pendulum.timezone('UTC')
+try:
+    tz = conf.get("core", "default_timezone")
+    if tz == "system":
+        utc = pendulum.local_timezone()
+    else:
+        utc = pendulum.timezone(tz)
+except Exception:
+    pass
 
 
 def setup_event_handlers(engine,
